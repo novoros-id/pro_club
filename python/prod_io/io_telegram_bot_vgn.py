75de5170a03455c54bdab17fa59fa85b9c5e1e07
novoros-id <novoros-id@inbox.ru>
2024-11-29 14:53:01 +0300

    add script

--- python/prod_io/io_telegram_bot_vgn.py
+++ python/prod_io/io_telegram_bot_vgn.py
@@ -1,5 +1,5 @@
 import config_vgn
-import io_universal
+import io_file_operation
 import telebot
 
 bot = telebot.TeleBot(config_vgn.token)
@@ -10,8 +10,5 @@ def start_handler(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-  print(message.from_user.username)
-  file_name = io_universal.sanitize_filename(message.from_user.username)
-  bot.reply_to(message, 'Эхо сообщения: ' + message.text + ' . file name ' + file_name)
-
+  io_file_operation.create_user (bot, message.chat.id, message.from_user.username)
 bot.polling()
